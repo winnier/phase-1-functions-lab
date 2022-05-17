b1312b324093764e00c1fdbdd1cae245adcc04ca
Winnie <wrm956@gmail.com>
2022-05-16 20:40:36 -0400

    commit tree 1975a5d953ca15c3992ab46f80c66bd15af3fe9e

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    return Math.abs(street - 42);
+}
+
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(startStreet, endStreet) {
+    return Math.abs(endStreet - startStreet) * 264
+}
+
+function calculatesFarePrice(startStreet, endStreet) {
+    let distance = distanceTravelledInFeet(startStreet, endStreet) - 400
+    if (distance <= 0) {
+        return 0
+    } else if (distance <= 1600) {
+       return distance * .02
+    } else if (distance <= 2100) {
+        return 25
+    } else if (distance > 2100) {
+        return "cannot travel that far"
+    }
+}
